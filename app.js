d38ee97e74cc0ec63138d84a105372aa2d376dd9
Suman Vernekar <sumanvernekarvernekar@gmail.com>
2026-08-27 20:08:58 +0530

    app.js modified

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const recipeForm = document.getElementById('recipeForm');
 const recipeList = document.getElementById('recipeList');
 const searchInput = document.getElementById('searchInput');
 
-const a =0;
+
 
 // Load recipes when page loads
 document.addEventListener('DOMContentLoaded', () => {
